36184dea6f7f81d1096a8b4452e85b705f9de2ae
Mehmet Eren Kaşıkçı <mehmeterenkasikci@gmail.com>
2023-08-22 21:30:38 +0300

    Redesign, Add Functions

---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Sınıflar;
+
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        Context c = new Context();
+
+        public ActionResult Index()
+        {
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
+        }
+        public ActionResult BlogDetay(int id)
+        {
+            var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+            return View(blogbul);
+        }
+    }
+}
