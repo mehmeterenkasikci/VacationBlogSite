768c8c9f35dd19e8194a8a506bfb64f830c6388e
Mehmet Eren Kaşıkçı <mehmeterenkasikci@gmail.com>
2023-08-23 23:03:27 +0300

    Add Functions Blog Page

--- TravelTripProje/Controllers/BlogController.cs
+++ TravelTripProje/Controllers/BlogController.cs
@@ -12,16 +12,23 @@ namespace TravelTripProje.Controllers
     {
         // GET: Blog
         Context c = new Context();
-
+        BlogYorum by = new BlogYorum();
         public ActionResult Index()
         {
-            var bloglar = c.Blogs.ToList();
-            return View(bloglar);
+
+            //var bloglar = c.Blogs.ToList();
+            by.Deger1 = c.Blogs.ToList();
+            by.Deger3 = c.Blogs.OrderByDescending(x=>x.ID).Take(5).ToList();
+            return View(by);
         }
+        
         public ActionResult BlogDetay(int id)
         {
-            var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
-            return View(blogbul);
+
+            //var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
+            return View(by);
         }
     }
 }
