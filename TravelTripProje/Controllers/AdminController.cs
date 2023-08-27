07880d617a045f0463e39594c355139f4ac5c6de
Mehmet Eren Kaşıkçı <mehmeterenkasikci@gmail.com>
2023-08-27 23:17:57 +0300

    Adjustments, Finish The Homepage, Admin Panel, Add Functions

---
+++ TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Sınıflar;
+
+
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+    }
+}
--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -36,5 +36,15 @@ namespace TravelTripProje.Controllers
             var deger = c.Blogs.ToList();
             return PartialView(deger);
         }
+        public PartialViewResult Partial4()
+        {
+            var deger = c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial5()
+        {
+            var deger = c.Blogs.Take(3).OrderByDescending(x => x.ID).ToList();
+            return PartialView(deger);
+        }
     }
 }
